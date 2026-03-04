6d220743190ca62ff96cfd22e86ec04bd387aa3a
Luiz Felipe <alucardlf1911@gmail.com>
2026-03-04 18:04:11 -0300

    Criando hr-worker

---
+++ hr-woker/src/test/java/luiz/oliveira/hr_woker/HrWokerApplicationTests.java
@@ -0,0 +1,13 @@
+package luiz.oliveira.hr_woker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrWokerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
