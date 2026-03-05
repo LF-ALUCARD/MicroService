1f3faf6003e1ebc2afab5ffa6b6043c23adacc01
Luiz Felipe <alucardlf1911@gmail.com>
2026-03-05 16:10:59 -0300

    Criando eureka server

--- hr-payroll/src/main/java/luiz/oliveira/hr_payroll/service/PaymentService.java
+++ hr-payroll/src/main/java/luiz/oliveira/hr_payroll/service/PaymentService.java
@@ -4,23 +4,27 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import luiz.oliveira.hr_payroll.entities.Payment;
 import luiz.oliveira.hr_payroll.entities.Worker;
-import luiz.oliveira.hr_payroll.feingclients.WorkerFeignClient;
 
 @Service
 public class PaymentService {
 
+	@Value("$hr-worker.host")
+	private String workerHost;
+
 	@Autowired
-	private WorkerFeignClient workerFeignClient;
-	
+	private RestTemplate rest;
+
 	public Payment getPaymet(Long workerId, Integer days) {
 		Map<String, String> url = new HashMap<>();
 		url.put("id", ""+workerId);
 
-		Worker worker = workerFeignClient.findById(workerId).getBody();
+		Worker worker = rest.getForObject(workerHost + "/worker/{id}", Worker.class, url);
 
 		return new Payment(worker.getName(), worker.getDailyIncome(), days);
 	}
