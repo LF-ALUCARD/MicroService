98542b741a81a86c38af6c6b9378c681dcf16ee0
Luiz Felipe <alucardlf1911@gmail.com>
2026-03-05 09:29:28 -0300

    Substituindo restTamplet por FeignClient

---
+++ hr-payroll/src/main/java/luiz/oliveira/hr_payroll/feingclients/WorkerFeignClient.java
@@ -0,0 +1,17 @@
+package luiz.oliveira.hr_payroll.feingclients;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import luiz.oliveira.hr_payroll.entities.Worker;
+
+@Component
+@FeignClient(name = "hr-worker", url = "localhost:8001/api", path = "/worker")
+public interface WorkerFeignClient {
+
+	@GetMapping("{id}")
+	ResponseEntity<Worker> findById(@PathVariable Long id);
+}
