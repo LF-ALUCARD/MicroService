08ea5b4854ee97fc378e414bb0b401e907440439
Luiz Felipe <alucardlf1911@gmail.com>
2026-03-06 15:33:34 -0300

    Hystrix com falhas

--- hr-payroll/src/main/java/luiz/oliveira/hr_payroll/controlles/PaymentController.java
+++ hr-payroll/src/main/java/luiz/oliveira/hr_payroll/controlles/PaymentController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+
 import luiz.oliveira.hr_payroll.entities.Payment;
 import luiz.oliveira.hr_payroll.service.PaymentService;
 
@@ -17,8 +19,13 @@ public class PaymentController {
 	@Autowired
 	private PaymentService service;
 	
+	@HystrixCommand(fallbackMethod = "getPaymentAlternative")
 	@GetMapping("{workerId}/days/{days}")
 	public ResponseEntity<Payment> getPayment(@PathVariable Long workerId, @PathVariable Integer days){
 		return ResponseEntity.ok().body(service.getPayment(workerId, days));		
+	}
+	
+	public ResponseEntity<Payment> getPaymentAlternative(Long workerId, Integer days){
+		return ResponseEntity.ok().body(new Payment("Luiz Felipe", 500.0, days));		
 	}
 }
