7b153d7b34305abcbdaa55479d0c1517feb7ca1d
Luiz Felipe <alucardlf1911@gmail.com>
2026-03-22 13:30:54 -0300

    Associando User com Role

---
+++ hr-user/src/main/java/luiz/oliveira/hr_user/entities/Role.java
@@ -0,0 +1,79 @@
+package luiz.oliveira.hr_user.entities;
+
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+@Entity
+@Table(name = "tb_role")
+public class Role implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String roleName;
+
+	@JsonIgnore
+	@ManyToMany
+	private Set<User> users = new HashSet<>();
+
+	public Role() {
+		super();
+	}
+
+	public Role(Long id, String roleName) {
+		super();
+		this.id = id;
+		this.roleName = roleName;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getRoleName() {
+		return roleName;
+	}
+
+	public void setRoleName(String name) {
+		this.roleName = name;
+	}
+
+	public Set<User> getUsers() {
+		return users;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Role other = (Role) obj;
+		return Objects.equals(id, other.id);
+	}
+
+}
