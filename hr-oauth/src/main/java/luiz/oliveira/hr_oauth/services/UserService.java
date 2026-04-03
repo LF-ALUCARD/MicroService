659ac5af338f6a268a586e32ea47cd920c6d83fc
Luiz Felipe Oliveira <alucardlf1911@gmail.com>
2026-04-03 13:54:16 -0300

    Login e geração de token JWT

--- hr-oauth/src/main/java/luiz/oliveira/hr_oauth/services/UserService.java
+++ hr-oauth/src/main/java/luiz/oliveira/hr_oauth/services/UserService.java
@@ -3,13 +3,16 @@ package luiz.oliveira.hr_oauth.services;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import luiz.oliveira.hr_oauth.entities.User;
 import luiz.oliveira.hr_oauth.feingclients.UserFeignClient;
 
 @Service
-public class UserService {
+public class UserService implements UserDetailsService{
 
 	private static Logger logger = LoggerFactory.getLogger(UserService.class);
 	
@@ -25,6 +28,20 @@ public class UserService {
 		}
 		
 		logger.info("E-mail found: " + email);
+		
+		return user;
+	}
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+User user = repositor.findByEmail(username).getBody();
+		
+		if(user == null) {
+			logger.error("E-mail not found " + username);
+			throw new UsernameNotFoundException("E-mail not found");
+		}
+		
+		logger.info("E-mail found: " + username);
 		
 		return user;
 	}
--- hr-user/src/main/java/luiz/oliveira/hr_user/HrUserApplication.java
+++ hr-user/src/main/java/luiz/oliveira/hr_user/HrUserApplication.java
@@ -1,15 +1,30 @@
 package luiz.oliveira.hr_user;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @EnableEurekaClient
 @SpringBootApplication
-public class HrUserApplication {
+public class HrUserApplication implements CommandLineRunner{
 
+	@Autowired
+	private BCryptPasswordEncoder passwordEncode;
+	
+	private Logger logger = LoggerFactory.getLogger(HrUserApplication.class);
+	
 	public static void main(String[] args) {
 		SpringApplication.run(HrUserApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		logger.info("Senha: " + passwordEncode.encode("123456"));	
 	}
 
 }
